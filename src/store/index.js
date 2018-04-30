3e85522e0733d06b733e5186aae72a77ea8c5890
Jan Pfeiffer <liberry@ymail.com>
2018-04-30 11:50:46 +0200

    Add dummy data

--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,28 @@ export default new Vuex.Store({
   },
   getters: {
     requests: state => state.requests
+    // dummy data in case it's needed
+    // [{
+    //   'id': 1,
+    //   'requester': 'kwilson0',
+    //   'request': 'convallis morbi odio odio elementum eu interdum eu tincidunt in leo'
+    // }, {
+    //   'id': 2,
+    //   'requester': 'rghiotto1',
+    //   'request': 'ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae nulla dapibus dolor vel est donec odio justo'
+    // }, {
+    //   'id': 3,
+    //   'requester': 'cgoodboddy2',
+    //   'request': 'primis in faucibus orci luctus et ultrices posuere cubilia curae nulla dapibus dolor vel est donec odio justo sollicitudin ut'
+    // }, {
+    //   'id': 4,
+    //   'requester': 'djendrach3',
+    //   'request': 'rutrum nulla tellus in sagittis dui vel nisl duis ac'
+    // }, {
+    //   'id': 5,
+    //   'requester': 'ccoombe4',
+    //   'request': 'massa id nisl venenatis lacinia aenean sit amet justo'
+    // }]
   },
   mutations: {
     addRequest (state, request) {
